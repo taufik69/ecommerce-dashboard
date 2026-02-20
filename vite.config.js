a8cad299cd745dd05b1b50ca8563ea35349193da
Protik Datta <protikdatta135@gmail.com>
2026-02-20 22:16:20 +0600

    removing base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
